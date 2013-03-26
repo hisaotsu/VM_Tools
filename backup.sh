8f1cbda092bf1bcf756d7c75fe8edb450afeab86
Hisao Tsujimura <hisao.tsujimura@gmail.com>
2013-03-27 00:44:39 +0900

    test version  20130327_2345

--- backup.sh
+++ backup.sh
@@ -3,37 +3,34 @@
 #
 # Virtual Machine Backup Script
 # 
-# 2012.07.21 Version 2.0 by Hisao Tsujimura
-# 2012.07.21 Version 2.1 by Hisao Tsujimura
-# 	Added to record return code from VBoxManage export
-# 2012.07.24 Version 2.2 by Hisao Tsujimura
-#	bug fix -- remove .old directory.
-# 2012.07.27 Version 2.3 by Hisao Tsujimura
-#	added backup one feature, which will ask 1 VM to backup
-#	  <syntax> ./backup.sh one
-# 2012.09.25 Version 2.4 by Hisao Tsujimura
-#	add start date and end date display
-# 2013.03.11 Version 2.5 by Hisao Tsujimura
-# 	add -ovf20 parameter to export in ovf20.
+# Copyright © 2012 - 2013 by Hisao Tsujimura
+#
+# 2013/03/26 Version 3.0 - rewrite of the tool
 #==============================================================
 #--------------------------------------------------------------
 # Variables
 #--------------------------------------------------------------
-export VERSION=2.5
+export VERSION=3.0
 export NAS_MOUNTPOINT=/Volumes/vm
 ##export NAS_MOUNTPOINT=/Volumes/export/vm
 export VM_STATE_DIR=$NAS_MOUNTPOINT/vmstates
 export TEMPFILE=$VM_STATE_DIR/tempfile.temp
-export LOG_FILE='./log_backup.txt'
+export LOG_FILE='./backup_sh.log'
+
 #--------------------------------------------------------------
-# functions
+# Functions - Logging function
 #--------------------------------------------------------------
+
 # keep log of the script
 verbose()
 {
 	echo $* 
 	echo `date +%Y/%m/%d_%H:%M:%S`': '$* >> $LOG_FILE
 }
+
+#--------------------------------------------------------------
+# Functions - cosmetic messages
+#--------------------------------------------------------------
 
 # title screen
 show_title()
@@ -44,26 +41,35 @@ show_title()
 	verbose '--------------------------------------------------------------'
 }
 
-# get list of all registerd virtual machines
+#--------------------------------------------------------------
+# Functions - VirtualBox related functions
+#--------------------------------------------------------------
+#
+# function : get_vm_list
+# arguments: none
+# usage    : get_vm_list
+# description:
+#  This function returns the name of registered virtual machine 
+#  names delimited by white spaces.
+
 get_vm_list()
 {
-##VBoxManage list vms | awk '{print $1}' | \
-## 		      awk '{printf("%s "),substr($0,2,length($0)-2);}' | \
-##		      sort -n
 VBoxManage list vms | sed 's/{.*}//' | \
 		      sed 's/\"//' | \
 		      sed 's/\"//' 
 }
 
-# show list of all registered virtual machines
-display_vm_list()
-{
-	verbose '  - currently registered virtual machines'
-	VBoxManage list vms | awk '{print $1}'
-}
-
-# function to check if the vm is running
-vm_running()
+#
+# function : vm_is_running
+# arguments: <VM name>
+# usage    : vm_is_running <VM name>
+# description:
+#  This function returns the followings.
+#	If the VM specified by VM is:
+#		running     - "true"
+#		not running - "false"
+#
+vm_is_running()
 {
 	state=`VBoxManage list runningvms | grep $1`
 	case $state in
@@ -74,67 +80,60 @@ vm_running()
 	esac
 }
 
-backup_vm()
+#--------------------------------------------------------------
+# Functions - state repository functions
+#--------------------------------------------------------------
+#
+# function : make_state_repository
+# arguments: none
+# usage    : make_state_repository
+# description:
+#  This function creates a repository based on the following
+#  variable preset make_state_repository().
+#  $VM_STATE_DIR
+
+make_state_repository()
 {
-	VM_name=$1
-	
-	STAT=`vm_running $VM_name`
-	if [ $STAT = 'true' ]
-	then
-		verbose '::: Virual Machine '$VM_name' is running.'
-		verbose '::: exiting...'
-		exit
-	fi
-
-	### make the backup of the old backup
-	#verbose '- moving current backup to old if any...'
-	# // 2012.07.24 bug fix
-	rm -rf $NAS_MOUNTPOINT/$VM_name.old 2> /dev/null
-	mv $NAS_MOUNTPOINT/$VM_name $NAS_MOUNTPOINT/$VM_name.old 2> /dev/null
-	verbose ':::: exporting: '$VM_name' ...'
-	verbose `date`
-	mkdir -p $NAS_MOUNTPOINT/$VM_name 2> /dev/null
-	VBoxManage export $VM_name --output $NAS_MOUNTPOINT/$VM_name/$VM_name.ovf --ovf20
-	RC=$?
-	if [ $RC != 0 ]
-	then
-		verbose '!!! export did not end normally. rc='$RC
-	fi
-
-	VBoxManage showvminfo $VM_name > $VM_STATE_DIR/$VM_name.state
+	mkdir -p $VM_STATE_DIR 2> /dev/null
 }
-#--------------------------------------------------------------
-# test code
-#--------------------------------------------------------------
-export FLAG=$1
 
-show_title
-echo '### DEBUG flag='$FLAG
+#
+# function : store_vm_state
+# arguments: <VM name>
+# usage    : store_vm_state <VM name>
+# description:
+#  This stores the Virtual Machines status and store in the 
+#  repository or $VM_STATE_DIR.
+#
+store_vm_state()
+{
+	VM_name=$1	#preserve the parameter passed.
+	
+	verbose '::: saving VM state…'
+	VBoxManage showvminfo $VM_name > $VM_STATE_DIR/$VM_name.state
+	verbose '::: stored VM state.'
+}
 
+# function : check_vm_state_update
+# arguments: <VM name>
+# usage    : check_vm_state_update <VM name>
+# description:
+#   This function compares the previous state of the VM
+#   stored in the repository with the current state of VM.
+#   If they are different, it returns "true," and if not, "false."
+check_vm_state_update()
+{
+	VM_name=$1	# preserve the parameter.
 
-# check if backup directory is ready.  if not, end the program...
-if [ ! -d $NAS_MOUNTPOINT  ]
-then
-	verbose ' - backup directory '$NAS_MOUNTPOINT ' is not mounted.'
-	verbose ' - exisiting.'
-	exit 16
-fi
+	### CHECK when the status does not exist.
 
-### get vm list and for each VM, compare with the last status of
-### vm with the current output.  If the status is any way different,
-### put them into the backup list
-VMS=`get_vm_list`
-mkdir $VM_STATE_DIR 2> /dev/null
-
-verbose '---- checking if each VM needs backup'
-for i in $VMS
-do
-	if [ ! -f $VM_STATE_DIR/$i.state ]
-	then 
-		BACKUP_LIST=$BACKUP_LIST' '$i
+	if [ !-f $VM_STATE_DIR/$VM_name.state ]
+	then
+		RC=true
 	fi
 
-	if [ -f $VM_STATE_DIR/$i.state ]
+	### CHECK when the status exist.
+	if [ -f $VM_STATE_DIR/$VM_name.state ]
 	then
 		VBoxManage showvminfo $i > $TEMPFILE 
 		STAT=`diff $TEMPFILE $VM_STATE_DIR/$i.state`
@@ -142,50 +141,101 @@ do
 		case $STAT in
 		'')
 			verbose '--- VM '$i' has no change.'
+			RC='false'
 			;;
 		*)
 			verbose '--- VM '$i' needs backup.'
-			BACKUP_LIST=$BACKUP_LIST' '$i
+			RC='true'
 			;;
 		esac
 	fi
-done
 
-###
-### backup one -- override BACKUP_LIST if "one" is specified
-###
-case $FLAG in
-'one')
-	verbose '=== You specified to back up only one VM.'
-	verbose '=== Which of the following VMs would you like to backup?'
-	echo $VMS
-	read THIS_VM
-	case $THIS_VM in
-	'')
-		continue;;
-	*)
-		STAT=`echo $VMS | grep $THIS_VM`
-		case $STAT in 
-		'')
-			continue;;
-		*)
-			BACKUP_LIST=$THIS_VM;;
-		esac
+	echo $RC
+}
+
+
+#--------------------------------------------------------------
+# Functions - checking environment
+#--------------------------------------------------------------
+# function : check_mount_point
+# arguments: none
+# usage    : check_mount_point
+# description:
+#  This function checks if $NAS_MOUNTPOINT exist.
+#  If it does, it returns true.  If not, it returns false.
+#
+check_mount_point()
+{
+
+if [ ! -d $NAS_MOUNTPOINT  ]
+then
+	verbose ' - backup directory '$NAS_MOUNTPOINT ' is not mounted.'
+	verbose ' - exiting.'
+	exit 16
+fi
+}
+
+
+#--------------------------------------------------------------
+# Function - backup
+#--------------------------------------------------------------
+# function : backup_this_vm
+# arguments: <VM name>
+# usage    : backup_this_vm
+# description:
+#   This function backup 1 VM.
+backup_this_vm()
+{
+	VM_name=$1	# preserve parameter
+
+	verbose '- backup: VM='$VM_name
+	
+	## If the VM is running, skip it.
+
+	VM_STATE=`vm_is_running $VM_name`
+
+	case $VM_STATE in
+	'false')
+		#since VM is NOT running, do main backup.
+		verbose ':::: moving old backup.'
+		rm -rf $NAS_MOUNTPOINT/$VM_name.old 2> /dev/null
+		mv $NAS_MOUNTPOINT/$VM_name $NAS_MOUNTPOINT/$VM_name.old 2> /dev/null
+
+		# now do actual export
+		
+		verbose ':::: exporting: '$VM_name' ...'
+		verbose `date`
+		mkdir -p $NAS_MOUNTPOINT/$VM_name 2> /dev/null
+		VBoxManage export $VM_name --output $NAS_MOUNTPOINT/$VM_name/$VM_name.ovf --ovf20
+		RC=$?
+		if [ $RC != 0 ]
+		then
+			verbose '!!! export did not end normally. rc='$RC
+		fi
+		;;
+	'true')
+		verbose ':::: '$VM_name 'is running - skipping.'
 		;;
 	esac
-	;;
-esac
 
-###
-### backup VMs
-###
+	# now store new VM state
+	store_vm_state $VM_name
+}
 
+#--------------------------------------------------------------
+#Main Routine
+#--------------------------------------------------------------
+
+
+show_title
 echo '-- backing up VMs (if any)'
 START_DATE=`date`
-for i in $BACKUP_LIST 
+
+for i in `get_vm_list`
 do
-	backup_vm $i
+	echo $i
 done
+
 END_DATE=`date`
 verbose '  start date='$START_DATE
 verbose '  end   date='$END_DATE
